991f1dd46b5b5b8bc8e104aee1eea1bbfe3b82bc
Nakamury1 <sophielamsova29@gmail.com>
2024-12-29 19:49:41 +0100

    Repository finales

--- src/Repository/ContenuPanierRepository.php
+++ src/Repository/ContenuPanierRepository.php
@@ -16,28 +16,28 @@ class ContenuPanierRepository extends ServiceEntityRepository
         parent::__construct($registry, ContenuPanier::class);
     }
 
-    //    /**
-    //     * @return ContenuPanier[] Returns an array of ContenuPanier objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+//    /**
+//     * @return ContenuPanier[] Returns an array of ContenuPanier objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
 
-    //    public function findOneBySomeField($value): ?ContenuPanier
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+//    public function findOneBySomeField($value): ?ContenuPanier
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
 }
